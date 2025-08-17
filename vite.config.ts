b15eaca3d04b15c97951e1646219ae0f3a60beab
Arora, Rahul <ar-rahul.arora@rakuten.com>
2025-08-17 15:00:49 +0530

    multiplayer mode room

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: 'globalThis',
+  },
   server: {
     proxy: {
       "/api": {
